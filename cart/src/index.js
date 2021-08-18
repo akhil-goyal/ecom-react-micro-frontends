e612f4f6080dec4362941eee51d64b6c1060703b
Akhil Goyal <akhilgoyal@akhils-mbp.lan>
2021-08-18 17:06:44 -0400

    Fixed the issue of faker module not being loaded in isolation mode.

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,5 +1,1 @@
-import faker from 'faker';
-
-let cartText = `You have ${faker.datatype.number()} items in your cart.`;
-
-document.querySelector('#cart-dev').innerHTML = cartText;
+import('./bootstrap');
